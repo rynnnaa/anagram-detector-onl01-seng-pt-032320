15ca501d6ef8eec842035b4a5471c86cc33324a1
Ryna Truong <rynatruong@outlook.com>
2020-05-04 16:22:03 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,6 +8,6 @@ class Anagram
   end
   
   def match(arr)
-    arr.select {|str| str.split("").sort == word.split("").sort}
+    arr.select {|str| str.split("").sort == word.split("").sort
   end
 end
