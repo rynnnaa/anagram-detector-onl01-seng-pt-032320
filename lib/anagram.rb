235a2c10dc226376b3d25377ce8cf760efd78a80
Ryna Truong <rynatruong@outlook.com>
2020-05-04 15:26:07 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require ('pry')
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word
+  end
+  
+  def match(arr)
+    arr.each do |word|
+      word.split("")}
+    if new_word.reverse == arr
+      arr << new_word
+    else 
+      return nil
+    end
+  end
+end
